f132e7e11313ade949a3358c23dd8eb37ca41578
Park Juhyung <majecty@gmail.com>
2022-02-19 23:43:37 +0900

    add text to diary

--- src/js/scenes/baekgol/diaryMain.vue
+++ src/js/scenes/baekgol/diaryMain.vue
@@ -1,13 +1,20 @@
 <script>
+import { sampleText } from "../../misc/sampleText";
+
 export default {
   name: "DiaryMain",
+  created() {
+    this.sampleText = sampleText.replace(/(?:\r\n|\r|\n)/g, '<br />');
+  },
 };
 </script>
 
 <template>
-<div cl>
-</div>
 <div id="diary" class="absolute">
+  <div id="diary-scrollable" class="absolute">
+    <p id="diary-text" v-html="sampleText">
+    </p>
+  </div>
 </div>
 </template>
 
@@ -20,5 +27,28 @@ export default {
   transform-origin: top left;
   top: 500px;
   left: 60px;
+}
+
+#diary-scrollable {
+  left: 200px;
+  right: 100px;
+  bottom: 100px;
+  top: 100px;
+  overflow-y: scroll;
+}
+
+ /* Hide scrollbar for Chrome, Safari and Opera */
+#diary-scrollable::-webkit-scrollbar {
+  display: none;
+}
+
+/* Hide scrollbar for IE, Edge and Firefox */
+#diary-scrollable {
+  -ms-overflow-style: none;  /* IE and Edge */
+  scrollbar-width: none;  /* Firefox */
+} 
+
+#diary-text {
+  font-size: 64px;
 }
 </style>
