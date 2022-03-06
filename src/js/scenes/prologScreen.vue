3102f3cba893b07f7df231ebe6b8df88a9429deb
Park Juhyung <majecty@gmail.com>
2022-03-06 20:32:48 +0900

    update prolog screen

--- src/js/scenes/prologScreen.vue
+++ src/js/scenes/prologScreen.vue
@@ -28,14 +28,30 @@ export default {
   <div id="prolog-screen-black-screen">
   </div>
   <VisibleRegion>
+		<div id="prolog-screen-poster-image" class="absolute" >
+		</div>
     <button id="prolog-screen-enter-button"
       @click="click"
       class="interactive-button">
     </button>
+    <div id="prolog-screen-description" class="absolute">
+      ✥입장 후 사운드가 재생됩니다✥
+    </div>
   </VisibleRegion>
 </template>
 
 <style>
+#prolog-screen-poster-image {
+  width: 2126px;
+	height: 3005px;
+  top: calc(50%	- 3005px / 2 - 100px);
+	left: calc(50% - 2126px / 2);
+  transform-origin: center;
+  bottom: 250px;
+  transform: scale(0.23);
+	background-image: url("/img/common/poster.png");
+}
+
 #prolog-screen-black-screen {
   background-color: rgba(0, 0, 0, 0.5);
   width: 100%;
@@ -47,9 +63,17 @@ export default {
   width: 311px;
   height: 122px;
   transform: scale(0.5);
-  transform-origin: top left;
-  /*top: 1200px;*/
+  transform-origin: center;
   bottom: 100px;
-  left: calc(50% - (311px / 4));
+  left: calc(50% - (311px / 2));
 }
+
+#prolog-screen-description {
+  font-family: "NanumSquare";
+  width: 100%;
+  bottom: 50px;
+  font-size: 30px;
+  color: white;
+  text-align: center;
+};
 </style>
