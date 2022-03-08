74a5da258fb4a5fd042b476f77c85e85eaa4ad8f
Park Juhyung <majecty@gmail.com>
2022-03-08 18:36:54 +0900

    move paw print button

--- src/js/scenes/bomi/pawPrintButton.vue
+++ src/js/scenes/bomi/pawPrintButton.vue
@@ -10,12 +10,14 @@ export default {
 
 <style>
 #paw-print-button {
+  --width: 1796;
+  --height: 1652;
+  --ratio: 0.03;
   background-image: url("/img/bomi/paw_print.png");
-  width: 1796px;
-  height: 1652px;
-  transform: scale(0.03);
-  transform-origin: top left;
+  aspect-ratio: calc(var(--width) / var(--height));
+  width: calc(1px * var(--width) * var(--ratio));
   top: 1110px;
-  left: 460px;
+  left: 360px;
+  background-size: 100%;
 }
 </style>
