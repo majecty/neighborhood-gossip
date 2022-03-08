53d36f0a6a7bf70d82527318618073b77d86d8a1
Park Juhyung <majecty@gmail.com>
2022-03-08 14:02:33 +0900

    add info page

---
+++ src/js/misc/infoPageText.js
@@ -0,0 +1,46 @@
+export const infoPageText = `
+<span class="info-page-text-title">
+이경주 
+Kyungjoo Lee
+( Kion Rhie ) 
+</span>
+
+-
+@kionrhie
+@jjuppon_ii__on__nocburn_alley
+kionrhie@gmail.com 
+- 
+2022. 03. 09 - 2022. 03. 27 
+서울시 서초구 방배로 42길 31-3 지하1층
+월, 화 휴관 / 1:00 pm - 7:00 pm 
+
+작가: 이경주  
+글: 정내희 
+사운드: 오정웅 [linktr.ee/ijskra] 
+웹제작: 박주형 [twitter/majecty] 
+디자인: 정소영 @shegochong
+촬영: 이해란 @haeran____ 
+
+Special thanks to: 
+흐루츠후라이랩(@fruitfrieslab), 김대유(@daeu.k) 
+
+&lt;Rumors in the Neighborhood&gt;
+@Whitenoise Bangbae 
+31-3 B1, Bangbae-ro 42gil, Seochogu, Seoul 
+Open Wed - Sun / 2:00 pm - 7:00 pm 
+
+Artist: Kyungjoo Lee (Kion Rhie) 
+Critic: Naehee Chung 
+Sound: Jungwoong Oh [linktr.ee/ijskra] 
+Web developer: Juhyung [twitter/majecty] 
+Design: Soyoung Chong @shegochong 
+Photograph: Haeran Lee @haeran____ 
+
+Special thanks to: 
+Fruitfrieslab(@fruitfrieslab), Daeyou Kim(@daeu.k) 
+
+copyright © 2022 all rights reserved
+
+<div id="info-page-poster">
+</div>
+`;
--- src/js/scenes/infoPage.vue
+++ src/js/scenes/infoPage.vue
@@ -1,11 +1,18 @@
 <script>
 import BackButton from "./common/backButton.vue";
+import { infoPageText } from "../misc/infoPageText";
+import VisibleRegion from "./visibleRegion.vue";
 
 export default {
   name: "InfoPage",
   emits: ["back"],
   components: {
     BackButton,
+    VisibleRegion,
+  },
+  created() {
+    /*this.text = infoPageText.replace(/(?:\r\n|\r|\n)/g, "<br />");*/
+    this.text = infoPageText;
   },
   methods: {
     back() {
@@ -16,5 +23,56 @@ export default {
 </script>
 
 <template>
+  <VisibleRegion>
+    <div id="info-page-text" v-html="text">
+    </div>
+  </VisibleRegion>
   <BackButton @back="back" />
 </template>
+
+<style>
+#info-page-text {
+  text-align: center;
+  color: white;
+  white-space: pre;
+  top: 100px;
+  bottom: 10px;
+  position: absolute;
+  width: 100%;
+  overflow-y: scroll;
+  display: flex;
+  flex-direction: column;
+  font-size: 20px;
+}
+
+.info-page-text-title {
+  font-family: "Hahmlet-Bold";
+  font-size: 30px;
+}
+
+ /* Hide scrollbar for Chrome, Safari and Opera */
+#info-page-text::-webkit-scrollbar {
+  display: none;
+}
+
+/* Hide scrollbar for IE, Edge and Firefox */
+#info-page-text {
+  -ms-overflow-style: none;  /* IE and Edge */
+  scrollbar-width: none;  /* Firefox */
+} 
+
+#info-page-poster {
+  background-image: url("/img/common/poster.png");
+  aspect-ratio: 2126/3005;
+  flex-shrink: 0;
+  width: 90%;
+  margin-left: 5%;
+  background-size: 100%;
+  margin-bottom: 100px;
+  /*width: 2126px;*/
+	/*height: 3005px;*/
+  /*transform-origin: center;*/
+  /*transform: scale(0.23);*/
+}
+
+</style>
